e6ff77d3e6b3cc371adcda0e79aad3a03769f63e
Edith Werbel <edith.werbel@gmail.com>
2020-05-03 21:54:30 -0400

    Updates

---
+++ precache-manifest.8ee3afcf3bd5602a96b3d113437131e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61f63fe9845dab87b91e06c4de617746",
+    "url": "/traineredith/projectFive/index.html"
+  },
+  {
+    "revision": "752b6a712953530d6b47",
+    "url": "/traineredith/projectFive/static/css/main.2a9e31f5.chunk.css"
+  },
+  {
+    "revision": "6f5d400118fc91137343",
+    "url": "/traineredith/projectFive/static/js/2.580bd1bb.chunk.js"
+  },
+  {
+    "revision": "4ae2e24854201e2c90f766e681fee893",
+    "url": "/traineredith/projectFive/static/js/2.580bd1bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "752b6a712953530d6b47",
+    "url": "/traineredith/projectFive/static/js/main.d83c46d6.chunk.js"
+  },
+  {
+    "revision": "cf27b4309d5e391e21d5",
+    "url": "/traineredith/projectFive/static/js/runtime-main.a1a77a75.js"
+  },
+  {
+    "revision": "8617225252b0f56e10540c79bec2daec",
+    "url": "/traineredith/projectFive/static/media/juno-logo.86172252.svg"
+  },
+  {
+    "revision": "9c4a16e739dd2ff7dbd5c085de4c6dd4",
+    "url": "/traineredith/projectFive/static/media/safiEmoji.9c4a16e7.png"
+  }
+]);
